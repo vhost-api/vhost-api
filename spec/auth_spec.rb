2425410106a330371326766c56f0429ce4539127
Thore Boedecker <me@foxxx0.de>
2016-06-19 00:11:30 +0200

    add failing logout test
    Signed-off-by: Thore Boedecker <me@foxxx0.de>

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -16,9 +16,19 @@ describe 'VHost-API Authentication' do
 
   it 'allows logging in with valid credentials' do
     clear_cookies
-    # p test_session.inspect
     post '/api/v1/auth/login', 'user' => { 'login' => 'max',
                                            'password' => 'muster' }
+    expect(last_response.redirect?).to be_truthy
+    follow_redirect!
+    expect(last_request.path).to eq('/')
+  end
+
+  it 'allows logging out from an active session' do
+    clear_cookies
+    post '/api/v1/auth/login', 'user' => { 'login' => 'max',
+                                           'password' => 'muster' }
+    # cookie = last_response.header['Set-Cookie'].split(';')[0]
+    get '/api/v1/auth/logout'
     expect(last_response.redirect?).to be_truthy
     follow_redirect!
     expect(last_request.path).to eq('/')
