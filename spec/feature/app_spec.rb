c138d86381b06649e1a1a08a84a41e18ab90f2de
Thore Boedecker <me@foxxx0.de>
2016-07-26 17:26:43 +0200

    fix app specs due to authentication rework
    Ref T20
    
    Signed-off-by: Thore Boedecker <me@foxxx0.de>

--- spec/feature/app_spec.rb
+++ spec/feature/app_spec.rb
@@ -2,10 +2,44 @@
 require File.expand_path '../../spec_helper.rb', __FILE__
 
 describe 'VHost-API Application' do
-  it 'redirects unauthenticated users to login page' do
-    get '/'
-    expect(last_response.redirect?).to be_truthy
-    follow_redirect!
-    expect(last_request.path).to eq('/login')
+  let(:appconfig) { YAML.load(File.read('config/appconfig.yml'))['test'] }
+
+  context 'by an unauthenticated user' do
+    it 'returns an an API unauthorized error' do
+      get '/'
+      expect(last_response.status).to eq(403)
+      expect(last_response.body).to eq(
+        return_json_pretty(
+          ApiResponseError.new(status_code: 403,
+                               error_id: 'unauthorized',
+                               message: unauthorized_msg).to_json
+        )
+      )
+    end
+  end
+
+  it 'returns an API Error for unexisting routes/endpoints' do
+    clear_cookies
+
+    testuser = create(:admin)
+    error_msg = 'requested resource does not exist'
+
+    get(
+      '/herp/derp/fooobar',
+      nil,
+      appconfig[:session][:key] => {
+        user_id: testuser.id,
+        group: Group.get(testuser.group_id).name
+      }
+    )
+
+    expect(last_response.status).to eq(404)
+    expect(last_response.body).to eq(
+      return_json_pretty(
+        ApiResponseError.new(status_code: 404,
+                             error_id: 'not found',
+                             message: error_msg).to_json
+      )
+    )
   end
 end
