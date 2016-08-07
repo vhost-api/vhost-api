051947881372e6005b352d930d7eee2106afed62
Thore Boedecker <me@foxxx0.de>
2016-08-07 11:58:03 +0200

    add enabled? checks for apikey, add comments
    Ref T27
    
    Signed-off-by: Thore Boedecker <me@foxxx0.de>

--- helpers/auth_helper.rb
+++ helpers/auth_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
+
+# Performs authentication and returns user if successful.
+#
+# @return [User]
 def authenticate!
   auth_header = request.env['HTTP_AUTHORIZATION']
   method, value = auth_header.try(:split, %r{\s+}) || [nil, '']
@@ -11,10 +15,20 @@ def authenticate!
   @user
 end
 
+# Decodes a base64 encoded secret and splits the
+# resulting string at the colon separator.
+#
+# @param value [String]
+# @return [Array(String)]
 def parse_base64_secret(value)
   Base64.decode64(value).strip.split(':')
 end
 
+# Performs authentication based on the HTTP Authorization
+# header for Basic method.
+#
+# @param value [String]
+# @return [User]
 def authenticate_password(value)
   credentials = parse_base64_secret(value)
   login = credentials.shift
@@ -26,13 +40,31 @@ def authenticate_password(value)
   raise AuthenticationError
 end
 
+# Performs authentication based on the HTTP Authorization
+# header for VHOSTAPI-KEY method.
+#
+# @param value [String]
+# @return [User]
 def authenticate_apikey(value)
   credentials = parse_base64_secret(value)
   user_id = credentials.shift.to_i
   apikey = credentials.shift
 
+  check_apikey_for_user(user_id, apikey)
+end
+
+# Performs validations on user and apikey.
+# Both need to map to existing records and need
+# to be enabled
+#
+# @param user_id [Fixnum]
+# @param apikey [String]
+# @return [User]
+def check_apikey_for_user(user_id, req_apikey)
   user = User.get(user_id)
-  raise AuthenticationError if user.nil? || !user.enabled?
-  return user if user.apikeys.map(&:apikey).include?(apikey)
+  apikey = Apikey.first(apikey: req_apikey)
+  raise AuthenticationError if user.nil? || !user.enabled? ||
+                               apikey.nil? || !apikey.enabled?
+  return user if user.apikeys.include?(apikey)
   raise AuthenticationError
 end
