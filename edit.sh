cb4beb9da59a453759b2a65109f8177fc5e3b31a
Thore Boedecker <me@foxxx0.de>
2016-08-22 14:00:29 +0200

    cleanup
    Signed-off-by: Thore Boedecker <me@foxxx0.de>

--- edit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC2046
-exec vim vhost_api_app.rb init.rb Gemfile Rakefile Guardfile config.ru $(find ./{config,database,app/models,app/controllers,app/helpers,app/public,app/policies,spec,tasks} -type f -not -iname 'jquery-*.js' -not -name '.keep' -not -iname '*.example.*' -not -iname '*.png'| tr '\n' ' ')
