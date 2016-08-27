03c24b6c50aa3f0ee36cb0e27dcde6b524912d8d
Thore Boedecker <me@foxxx0.de>
2016-08-27 13:24:05 +0200

    prevent exception by using #to_i
    Signed-off-by: Thore Boedecker <me@foxxx0.de>

--- app/policies/package_policy.rb
+++ app/policies/package_policy.rb
@@ -115,7 +115,7 @@ class PackagePolicy < ApplicationPolicy
       next unless k.to_s =~ %r{^quota_}
       remaining = check_quota_prop(k)
       mult = record.users.size
-      return false unless remaining >= ((v - record.send(k)) * mult)
+      return false unless remaining >= ((v - record.send(k).to_i) * mult)
     end
     true
   end
