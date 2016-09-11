89cecdb74250aa9d27c8836ecc0aba943e17dbbd
Sebastian Rakel <sebastian@devunit.eu>
2016-09-11 18:36:31 +0200

    Mailsource: provide relationships by static method

--- app/models/mailsource.rb
+++ app/models/mailsource.rb
@@ -27,12 +27,16 @@ class MailSource
     self.updated_at = Time.now.to_i
   end
 
+  def self.relationships
+    { domain: { only: [:id, :name] },
+      mail_accounts: { only: [:id, :email] } }
+  end
+  
   # @param options [Hash]
   # @return [Hash]
   def as_json(options = {})
     defaults = { exclude: [:domain_id],
-                 relationships: { domain: { only: [:id, :name] },
-                                  mail_accounts: { only: [:id, :email] } } }
+                 relationships: relationships }
 
     super(model_serialization_opts(defaults: defaults, options: options))
   end
