7687da76b1593385af493f1f23fdc621fa8b2bc3
Thore Boedecker <me@foxxx0.de>
2017-02-02 23:07:04 +0100

    add yard:graph rake task to generate dep graph
    Signed-off-by: Thore Boedecker <me@foxxx0.de>

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -3,10 +3,26 @@ begin
   require 'yard'
 
   YARD::Rake::YardocTask.new do |t|
-    t.files = ['./controllers/**/*.rb',
-               './helpers/**/*.rb',
-               './models/**/*.rb',
-               './policies/**/*.rb']
+    t.files = ['./api/**/.rb',
+               './lib/**/*.rb']
+  end
+
+  namespace :yard do
+    desc 'renders dependency graph'
+    task graph: :yard do
+      puts "\n\ngenerating graph...\n"
+      yard_cmd = 'yard graph --protected --private --full --dependencies'
+
+      # TODO: yard graph renders identifiers with ".self", which dot flags as
+      #   syntax errors. not sure what to do about them or if removing them
+      #   actually breaks the manipulated nodes, but it seems to work somehow.
+      sed_cmd = 'sed s/\.self//g'
+
+      dot_cmd = 'dot -Tjpg -o ./doc/yardoc/yard_graph.jpg'
+
+      `#{yard_cmd} | #{sed_cmd} | #{dot_cmd}`
+      puts 'done.'
+    end
   end
 
 rescue LoadError
